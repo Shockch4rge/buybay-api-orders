3f6e5c8599d940c1a1fa4e7692a4e439a22d8c2c
Favian <favouriteteo@gmail.com>
2023-02-08 22:09:31 +0800

    refactor: only expose 'show' route for orders

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix("orders")->group(function () {
-    Route::post("/", [OrderController::class, "store"]);
-    Route::get("/{id}", [OrderController::class, "show"]);
-});
+Route::apiResource("orders", OrderController::class)->only(["show"]);
 
 Route::get("/user/{id}/orders", [OrderController::class, "userOrders"]);
 Route::get("/user/{id}/sales", [OrderController::class, "userSales"]);
